e4bb5a8bbd19557d8a5335783511c8614d98b8fb
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-12-14 13:40:47 -0330

    build: add figterPreview, relesed this fn

--- src/javascript/components/fighterPreview.js
+++ src/javascript/components/fighterPreview.js
@@ -1,17 +1,5 @@
 import createElement from '../helpers/domHelper';
 
-export function createFighterPreview(fighter, position) {
-    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
-    const fighterElement = createElement({
-        tagName: 'div',
-        className: `fighter-preview___root ${positionClassName}`
-    });
-
-    // todo: show fighter info (image, name, health, etc.)
-
-    return fighterElement;
-}
-
 export function createFighterImage(fighter) {
     const { source, name } = fighter;
     const attributes = {
@@ -26,4 +14,38 @@ export function createFighterImage(fighter) {
     });
 
     return imgElement;
+}
+
+export function createFighterPreview(fighter, position) {
+    const positionClassName = position === 'right' ? 'fighter-preview___right' : 'fighter-preview___left';
+    const fighterElement = createElement({
+        tagName: 'div',
+        className: `fighter-preview___root ${positionClassName}`
+    });
+
+    // todo: show fighter info (image, name, health, etc.)
+    if (!fighter) {
+        return fighterElement;
+    }
+    const { name, attack, defense, health } = fighter;
+
+    const fighterImage = createFighterImage(fighter);
+    const fighterDetails = createElement({
+        tagName: 'div',
+        className: 'fighter-preview___detail'
+    });
+
+    const fighterName = createElement({ tagName: 'h3' });
+    fighterName.innerText = name;
+    const fighterHealth = createElement({ tagName: 'p' });
+    fighterHealth.innerText = `Health: ${health}`;
+    const fighterAttack = createElement({ tagName: 'p' });
+    fighterAttack.innerText = `Attack: ${attack}`;
+    const fighterDefense = createElement({ tagName: 'p' });
+
+    fighterDefense.innerText = `Defense: ${defense}`;
+    fighterDetails.append(fighterName, fighterHealth, fighterAttack, fighterDefense);
+    fighterElement.append(fighterImage, fighterDetails);
+
+    return fighterElement;
 }
