0214d1084c3c21c1372268eee14056317f0ac035
Vitaliy Beyar <37002507+nightven@users.noreply.github.com>
2023-12-14 13:46:35 -0330

    build: showWinnerModal, created logick for view winer fighter

--- src/javascript/components/modal/winner.js
+++ src/javascript/components/modal/winner.js
@@ -1,5 +1,23 @@
+import createElement from '../../helpers/domHelper';
+import { createFighterImage } from '../fighterPreview';
 import showModal from './modal';
 
 export default function showWinnerModal(fighter) {
     // call showModal function
+    const bodyElement = createElement({ tagName: 'div', className: 'modal-winner' });
+
+    const fighterImage = createFighterImage(fighter);
+
+    bodyElement.append(fighterImage);
+
+    function restart() {
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
+    }
+
+    showModal({
+        title: `${fighter.name} is the winner!`,
+        bodyElement,
+        onClose: () => restart()
+    });
 }
